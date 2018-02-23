a6c6f3f34a17e4157763ab0b8432f3fc30449ba6
browlm13 <browlm13@gmail.com>
2018-02-22 20:49:08 -0600

    Update __main__.py

--- detecting_objects/__main__.py
+++ detecting_objects/__main__.py
@@ -6,9 +6,11 @@ import os
 import glob
 import cv2
 import json
+import PIL.Image as Image
+import numpy as np
 
 # my lib
-from image_evaluator.src import image_evaluator
+#from image_evaluator.src import image_evaluator
 
 # logging
 logging.basicConfig(level=logging.INFO)
@@ -19,17 +21,69 @@ logger = logging.getLogger(__name__)
 # Test accuracy by writing new images
 #
 
-def write_image_for_accuracy_test(output_directory_path, image_file_name, image_np, selected_items):
+def write_frame_for_accuracy_test(output_directory_path, frame, image_np):
+	image_file_name = "frame_%d.JPEG" % frame 
+	output_file = os.path.join(output_directory_path, image_file_name)
+	cv2.imwrite(output_file, image_np)
 
-  for item in selected_items:
 
-    #test coors acuracy
-    (left, right, top, bottom) = item['box']
-    cv2.rectangle(image_np,(left,top),(right,bottom),(0,255,0),3)
+#list of 4 coordanates for box
+def draw_box_image_np(image_np, box):
+	(left, right, top, bottom) = box
+	cv2.rectangle(image_np,(left,top),(right,bottom),(0,255,0),3)
+	return image_np
 
-  #write
-  output_file = os.path.join(output_directory_path, image_file_name)
+def draw_all_boxes_image_np(image_np, image_info):
+	for item in image_info['image_items_list']:
+		draw_box_image_np(image_np, item['box'])
+	return image_np
+
+def load_image_np(image_path):
+	#non relitive path
+	script_dir = os.path.dirname(os.path.abspath(__file__))
+	image = Image.open(os.path.join(script_dir, image_path))
+	(im_width, im_height) = image.size
+	image_np = np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+
+	return image_np
+
+def filter_minimum_score_threshold(image_info_bundel, min_score_thresh):
+	filtered_image_info_bundel = {}
+	for image_path, image_info in image_info_bundel.items():
+		filtered_image_info_bundel[image_path] = image_info
+		filtered_image_items_list = []
+		for item in image_info['image_items_list']:
+			if item['score'] > min_score_thresh:
+				filtered_image_items_list.append(item)
+		filtered_image_info_bundel[image_path]['image_items_list'] = filtered_image_items_list
+	return filtered_image_info_bundel
+
+def filter_selected_categories(image_info_bundel, selected_categories_list):
+	filtered_image_info_bundel = {}
+	for image_path, image_info in image_info_bundel.items():			
+		filtered_image_info_bundel[image_path] = image_info
+		filtered_image_items_list = []
+		for item in image_info['image_items_list']:
+			if item['class'] in selected_categories_list:
+				filtered_image_items_list.append(item)
+		filtered_image_info_bundel[image_path]['image_items_list'] = filtered_image_items_list
+	return filtered_image_info_bundel		
+
+"""
+def write_frame_for_accuracy_test(output_directory_path, frame, image_np, image_info):
+
+	image_file_name = "frame_%d.JPEG" % frame 
+
+	for item in image_info:
+
+		#test coors acuracy
+		(left, right, top, bottom) = item['box']
+		cv2.rectangle(image_np,(left,top),(right,bottom),(0,255,0),3)
+
+	#write
+	output_file = os.path.join(output_directory_path, image_file_name)
   cv2.imwrite(output_file, image_np)
+ """
 
 #saving image_evaluator evaluations
 def save_image_directory_evaluations(image_directory_dirpath, image_boolean_bundel_filepath, image_info_bundel_filepath, model_list, bool_rule):
@@ -56,6 +110,28 @@ def load_image_info_bundel(image_info_bundel_filepath):
 		d = json.load(json_data)
 	return d
 
+def get_frame_path_dict(video_frames_dirpath):
+
+	# get path to each frame in video frames directory
+	image_path_list = glob.glob(video_frames_dirpath + "/*")
+
+	frame_path_dict = []
+	for path in image_path_list:
+
+		#get filename
+		filename = os.path.basename(path)
+
+		#strip extension
+		filename_wout_ext = filename.split('.')[0]
+		
+		#frame_number
+		frame = int(filename_wout_ext.split('_')[1])
+
+		frame_path_dict.append((frame, path))
+
+	return dict(frame_path_dict)
+
+
 if __name__ == '__main__':
 
 	#
@@ -64,6 +140,9 @@ if __name__ == '__main__':
 
 	#video frames diretory (basketball_225.JPEG - basketball_323.JPEG)
 	video_frames_dirpath = "/Users/ljbrown/Desktop/StatGeek/object_detection/video_frames/"
+
+	#output images directory for checking
+	output_image_directory = "/Users/ljbrown/Desktop/StatGeek/object_detection/output_images"
 
 	#image_boolean_bundel and image_info_bundel file paths for quick access
 	image_boolean_bundel_filepath = "/Users/ljbrown/Desktop/StatGeek/object_detection/image_evaluator_output/image_boolean_bundel.json"
@@ -81,6 +160,26 @@ if __name__ == '__main__':
 	
 	#load saved image_info_bundel
 	image_info_bundel = load_image_info_bundel(image_info_bundel_filepath)
-	
+
+	#filter selected categories and min socre
+	selected_categories_list = ['basketball', 'person']
+	min_score_thresh = 45.0
+	image_info_bundel = filter_selected_categories(filter_minimum_score_threshold(image_info_bundel, min_score_thresh), selected_categories_list)
+
+	#get frame image paths in order
+	frame_path_dict = get_frame_path_dict(video_frames_dirpath)
 
 
+	#
+	# test load and write frame
+	#
+
+	first_frame = 225
+	first_frame_path = frame_path_dict[first_frame]
+	first_frame_image_np = load_image_np(first_frame_path)
+	first_frame_image_info = image_info_bundel[first_frame_path]
+
+	#test draw boxes
+	first_frame_image_np = draw_all_boxes_image_np(first_frame_image_np, first_frame_image_info)
+
+	write_frame_for_accuracy_test(output_image_directory, first_frame, first_frame_image_np)
