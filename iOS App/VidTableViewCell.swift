be0fbded5d874818eb3d3cda6d4b38c814bc5a5d
Preston <preston@Prestons-Mac.local>
2018-03-06 18:06:25 -0600

    Added what I think are the proper files to begin developing the GUI for the iOS app.

---
+++ iOS App/VidTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VidTableViewCell.swift
+//  StatGeekAR
+//
+//  Created by Preston on 3/6/18.
+//
+
+import UIKit
+
+class VidTableViewCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
